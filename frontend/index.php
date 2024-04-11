8ee7f3134395b98b1f31bf0e130439e6abb23edd
AngelHameltonYacapin <yacapinton@gmail.com>
2024-04-11 16:19:07 +0800

    inedit ni ton ulit yung title

--- frontend/index.php
+++ frontend/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document Inetdit ni TOn</title>
+    <title>Document Inetdit ni TOn inedit ulit</title>
 </head>
 <body>
     
