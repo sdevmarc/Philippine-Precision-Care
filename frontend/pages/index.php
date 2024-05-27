ad5764ccf0a200cd47314b7732083773230f3917
Marc Edison <suanieeee@yahoo.com>
2024-05-28 01:52:02 +0800

    Update index.php

---
+++ frontend/pages/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PPC | Philippine Precision Care</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="./css//Home.css">
+    <link rel="stylesheet" href="../index.css">
+</head>
+
+<body>
+    <div class="w-full h-5 bg-[#a30000] flex justify-center items-center">
+        <h1 class='text-white font-extrabold text-xs tracking-widest'>
+           customercare@philippineprecisioncare.com
+        </h1>
+    </div>
+    <header class="sticky top-0 w-full h-[5rem] px-[10rem] flex justify-between items-center z-[2] bg-white">
+        <div class="overlow-hidden w-[30%] h-full ">
+            <img src='../assets/logo-no-background.png' alt="Logo" class="object-contain w-full h-full">
+        </div>
+        <div class="w-[70%] h-full flex justify-end items-center gap-[2rem]">
+            <a href="" class="text-[1rem] font-[400] text-black">
+                Home
+            </a>
+            <a href="" class="text-[1rem] font-[400] text-black">
+                Services
+            </a>
+            <a href="" class="text-[1rem] font-[400] text-black">
+                Team & Culture
+            </a>
+            <a href="" class="text-[1rem] font-[400] text-black">
+                Appointment
+            </a>
+            <a href="" class="text-[1rem] text-white font-[400] text-black bg-[#a30000] px-[1rem] py-[.3rem] duration-300 ease hover:scale-[.98] hover:opacity-[.7]">
+                Contact Us
+            </a>
+        </div>
+    </header>
+    <div class="hero relative w-full h-[70dvh] flex flex-col justify-center items-center">
+        <h1 class="text-white font-[700] text-[6rem] z-[1]">Don't take risks</h1>
+        <p class="text-white font-[700] text-[2rem] z-[1]">Get tested by a medical facility</p>
+    </div>
+    <div class="w-full py-[5rem] bg-[#ffffff] px-[2rem] sm:px-[5rem] md:px-[10rem] lg-[20rem] flex justify-center items-center gap-[15rem]">
+        <div class="overlow-hidden w-[23%] h-[30rem] flex justiy-ccenter items-center">
+            <img src="https://i0.wp.com/singaporediagnostics.com/v2021/wp/wp-content/uploads/2021/08/home-2.jpg?fit=424%2C580&ssl=1" alt="Pic" class="object-cover w-full h-full">
+        </div>
+        <div class="w-[40%] flex flex-col justify-center items-start gap-[1rem]">
+            <h1 class='font-bold text-[.8rem] sm:text-[1rem] md:text-[1.5rem] text-[#c10000]'>
+                About Us
+            </h1>
+            <h1 class='font-bold text-[1rem] sm:text-[2rem] md:text-[2.5rem] text-black text-justify'>
+                The fastest-growing medical laboratory in the Philippines
+            </h1>
+            <p class='w-full text-[.8rem] sm:text-[1rem] md:text-[1.3rem] text-center font-[500] text-justify'>
+                We service the laboratory requirements of up to 300 different institutions nationwide, from hospitals, to the largest clinic chains, to free-standing physician clinics. We're also the only CAP Accredited tertiary laboratory in the Philippines – putting us alongside the world’s best laboratories.</p>
+            <div class="w-full px-[1rem] py-[.5rem] flex flex-col gap-[.5rem]">
+                <p class='w-full text-[.8rem] sm:text-[1rem] md:text-[1.3rem] text-center font-[500] text-justify text-ellipsis overflow-hidden'>
+                    ✔ 6 locations across the Philippines
+                </p>
+                <p class='w-full text-[.8rem] sm:text-[1rem] md:text-[1.3rem] text-center font-[500] text-justify text-ellipsis overflow-hidden'>
+                    ✔ Over 300 healthcare institution partners nationwide
+                </p>
+                <p class='w-full text-[.8rem] sm:text-[1rem] md:text-[1.3rem] text-center font-[500] text-justify text-ellipsis overflow-hidden'>
+                    ✔ A CAP Accredited tertiary laboratory
+                </p>
+            </div>
+        </div>
+    </div>
+    <footer class="w-full h-[40dvh] bg-[#a30000] py-[2rem] px-[1rem] sm:px-[2rem] md-px-[3rem] flex flex-col justify-center items-center gap-[1rem]">
+        <h1 class='font-[500] text-[.5rem] sm:text-[.8rem] md:text-[1rem] text-[#fff] tracking-[.5rem] sm:tracking-[.7rem] md:tracking-[1rem]'>
+            CONNECT WITH US
+        </h1>
+        <div class="flex gap-[2rem]">
+            <a href="https://www.facebook.com/kantokusina" class="text-[#fff] text-[2rem]" target='_blank'>
+                <FacebookIcon />
+            </a>
+            <a href="mailto:jactubaran07@gmail.com" class="text-[#fff] text-[2rem]">
+                <EmailIcon />
+            </a>
+
+        </div>
+        <div class="text-[#fff] text-[.5rem] sm:text-[.6rem] md:text-[.8rem] text-center">
+            <p> Copyright &copy; 2024 Philippine Precision Care, All Rights Reserved.</p>
+        </div>
+    </footer>
+</body>
+
+</html>
