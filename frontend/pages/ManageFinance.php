857d1774f05515c40919b7898c634f4636895d7c
Marc Edison <suanieeee@yahoo.com>
2024-05-29 01:54:56 +0800

    chore: Update ManageFinance.php with improved table layout and edit form functionality

--- frontend/pages/ManageFinance.php
+++ frontend/pages/ManageFinance.php
@@ -54,9 +54,102 @@
             </button>
         </div>
     </header>
-    <div class="w-full h-[93vh] flex flex-col justify-center items-center gap-[1rem]">
-        <h1 class="text-black font-[700] text-[5rem]">Finance</h1>
-    </div>s
+    <div class="w-full h-[93vh] flex flex-col justify-start items-center px-[5rem]">
+        <div class="w-full h-[10%] flex items-center">
+            <h1 class="text-black text-[2rem] font-[700]">MANAGE FINANCE</h1>
+        </div>
+        <div class="overflow-auto w-full h-[88%] flex flex-col justify-start items-center">
+            <table class="w-full table-fixed">
+                <thead class="sticky top-0 h-[3rem] bg-[#a30000]">
+                    <tr class="border border-black text-black">
+                        <th class="border border-black text-white">ID</th>
+                        <th class="border border-black text-white">SAMPLE NAME</th>
+                        <th class="border border-black text-white">SAMPLE NAME</th>
+                        <th class="border border-black text-white">SAMPLE NAME</th>
+                        <th class="border border-black text-white">SAMPLE NAME</th>
+                        <th class="border border-black text-white">ACTIONS</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="border border text-black">
+                        <td class="border border-black text-black px-[1rem] text-center truncate">TTESTssssssssss ssssssssssssss ssssssssssss IDssssssssssssss</td>
+                        <td class="border border-black text-black px-[1rem] text-center truncate">TEST NAMEs</td>
+                        <td class="border border-black text-black px-[1rem] text-center truncate">TEST NAME</td>
+                        <td class="border border-black text-black px-[1rem] text-center truncate">TEST NAME</td>
+                        <td class="border border-black text-black px-[1rem] text-center truncate">TEST NAME</td>
+                        <td class="border border-black text-black px-[1rem] text-center truncate">
+                            <button onclick="openEditForm()" class="px-[1.5rem] py-[.3rem] mx-[.2rem] my-[.4rem] bg-[#3076f0] text-[.9rem] text-white font-[600] rounded-xl duration-300 ease hover:scale-[.98] hover:opacity-[.6]">
+                                Edit
+                            </button>
+                            <button class="px-[1.5rem] py-[.3rem] mx-[.2rem] my-[.4rem] bg-[#de5021] text-[.9rem] text-white font-[600] rounded-xl duration-300 ease hover:scale-[.98] hover:opacity-[.6]">
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div id="editFormOverlay" class="hidden absolute top-0 left-0 w-full h-screen bg-[#000] bg-opacity-[.3] flex justify-center items-center z-[1]">
+        <form id="editForm" action="" class="relative w-[70rem] h-[35rem] bg-white rounded-xl shadow-[0_10px_15px_-3px_rgba(0,0,0,0.1)] flex flex-col justify-start items-center">
+            <div onclick="closeEditForm()" class="absolute top-[-1rem] right-[-1rem] bg-[#9525f7] w-[3rem] h-[3rem] rounded-[50%] flex justify-center items-center text-white font-bold cursor-pointer duration-300 ease hover:scale-[.98] hover:opacity-[.6]">
+                x
+            </div>
+            <div class="overflow-auto w-full flex flex-col gap-[1rem]  px-[1.5rem] py-[1.5rem]">
+                <h1 class="text-[1.5rem] font-[700] text-black">Edit Sample</h1>
+                <div class="w-full flex flex-col justiy-start items-start gap-[.7rem]">
+                    <h1 class="text-black text-[1rem] font-[600]">Sample Name</h1>
+                    <input type="text" placeholder="Enter the sample..." class="w-full h-[3rem] px-[1rem] border border-black rounded-xl outline-none">
+                </div>
+                <div class="w-full flex flex-col justiy-start items-start gap-[.7rem]">
+                    <h1 class="text-black text-[1rem] font-[600]">Sample Name</h1>
+                    <input type="text" placeholder="Enter the sample..." class="w-full h-[3rem] px-[1rem] border border-black rounded-xl outline-none">
+                </div>
+                <div class="w-full flex flex-col justiy-start items-start gap-[.7rem]">
+                    <h1 class="text-black text-[1rem] font-[600]">Sample Name</h1>
+                    <input type="text" placeholder="Enter the sample..." class="w-full h-[3rem] px-[1rem] border border-black rounded-xl outline-none">
+                </div>
+                <div class="w-full flex flex-col justiy-start items-start gap-[.7rem]">
+                    <h1 class="text-black text-[1rem] font-[600]">Sample Name</h1>
+                    <input type="text" placeholder="Enter the sample..." class="w-full h-[3rem] px-[1rem] border border-black rounded-xl outline-none">
+                </div>
+                <div class="w-full flex flex-col justiy-start items-start gap-[.7rem]">
+                    <h1 class="text-black text-[1rem] font-[600]">Sample Name</h1>
+                    <input type="text" placeholder="Enter the sample..." class="w-full h-[3rem] px-[1rem] border border-black rounded-xl outline-none">
+                </div>
+                <div class="w-full flex flex-col justiy-start items-start gap-[.7rem]">
+                    <h1 class="text-black text-[1rem] font-[600]">Sample Name</h1>
+                    <input type="text" placeholder="Enter the sample..." class="w-full h-[3rem] px-[1rem] border border-black rounded-xl outline-none">
+                </div>
+                <div class="w-full flex flex-col justiy-start items-start gap-[.7rem]">
+                    <h1 class="text-black text-[1rem] font-[600]">Sample Name</h1>
+                    <input type="text" placeholder="Enter the sample..." class="w-full h-[3rem] px-[1rem] border border-black rounded-xl outline-none">
+                </div>
+                <button type="submit" class="w-full py-[.7rem] bg-[#a30000] text-white rounded-xl duration-300 ease hover:scale-[.98] hover:opacity-[.6]">
+                    SUBMIT
+                </button>
+            </div>
+        </form>
+    </div>
+
+    <script>
+        const editFormOverlay = document.getElementById('editFormOverlay');
+        const editForm = document.getElementById('editForm');
+
+        function openEditForm() {
+            editFormOverlay.style.display = 'flex';
+        }
+
+        function closeEditForm() {
+            editFormOverlay.style.display = 'none';
+        }
+
+        const editButtons = document.querySelectorAll('.edit-button');
+
+        editButtons.forEach(button => {
+            button.addEventListener('click', openEditForm);
+        });
+    </script>
 </body>
 
 </html>
