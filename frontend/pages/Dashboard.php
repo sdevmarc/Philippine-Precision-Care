a0ef75a2bab7c79f148ac71897324b10f05046c6
Marc Edison <suanieeee@yahoo.com>
2024-05-29 02:22:28 +0800

    chore: Update HR content styling in Dashboard.php

--- frontend/pages/Dashboard.php
+++ frontend/pages/Dashboard.php
@@ -20,10 +20,8 @@
                 <button class="text-[1rem] font-[400] text-black">
                     Human Resource
                 </button>
-                <div
-                    class="hr-content absolute top-[1.5rem] w-[13rem] left-[-2rem] hidden bg-[#a30000] rounded-xl py-[1rem] px-[1rem]">
-                    <a href="./ManageEmployees.php"
-                        class="text-[.9rem] font-[400] text-white font-[600] duration-300 ease hover:bg-black w-full px-[.5rem] py-[.5rem] rounded-xl text-center">
+                <div class="hr-content absolute top-[1.5rem] w-[13rem] left-[-2rem] hidden bg-[#a30000] rounded-xl py-[.5rem] px-[.5rem]">
+                    <a href="./ManageEmployees.php" class="text-[.9rem] font-[400] text-white font-[600] duration-300 ease hover:bg-black w-full px-[.5rem] py-[.5rem] rounded-xl text-center">
                         Manage Employees
                     </a>
                 </div>
@@ -33,10 +31,8 @@
                 <button class="text-[1rem] font-[400] text-black">
                     Finance
                 </button>
-                <div
-                    class="hr-content absolute top-[1.5rem] w-[13rem] left-[-4rem] hidden bg-[#a30000] rounded-xl py-[1rem] px-[1rem]">
-                    <a href="./ManageFinance.php"
-                        class="text-[.9rem] font-[400] text-white font-[600] duration-300 ease hover:bg-black w-full px-[.5rem] py-[.5rem] rounded-xl text-center">
+                <div class="hr-content absolute top-[1.5rem] w-[13rem] left-[-4rem] hidden bg-[#a30000] rounded-xl py-[.5rem] px-[.5rem]">
+                    <a href="./ManageFinance.php" class="text-[.9rem] font-[400] text-white font-[600] duration-300 ease hover:bg-black w-full px-[.5rem] py-[.5rem] rounded-xl text-center">
                         Manage Finance
                     </a>
                 </div>
@@ -46,77 +42,18 @@
                 <button class="text-[1rem] font-[400] text-black">
                     Appointment
                 </button>
-                <div
-                    class="hr-content absolute top-[1.5rem] w-[13rem] left-[-3rem] hidden bg-[#a30000] rounded-xl py-[1rem] px-[1rem]">
-                    <a href="./ManageAppointments.php"
-                        class="text-[.9rem] font-[400] text-white font-[600] duration-300 ease hover:bg-black w-full px-[.5rem] py-[.5rem] rounded-xl text-center">
+                <div class="hr-content absolute top-[1.5rem] w-[13rem] left-[-3rem] hidden bg-[#a30000] rounded-xl py-[.5rem] px-[.5rem]">
+                    <a href="./ManageAppointments.php" class="text-[.9rem] font-[400] text-white font-[600] duration-300 ease hover:bg-black w-full px-[.5rem] py-[.5rem] rounded-xl text-center">
                         Manage Appointments
                     </a>
                 </div>
             </div>
-
-            <button id="logoutButton"
-                class="text-[1rem] text-white font-[400] text-black bg-[#a30000] px-[1rem] py-[.3rem] duration-300 ease hover:scale-[.98] hover:opacity-[.7]">
-                <a href="../../backend/logout.php" style="text-decoration: none; color: white;">Logout</a>
-            </button>
-
+            <form action="../../backend/logout.php">
+                <button class="text-[1rem] text-white font-[400] text-black bg-[#a30000] px-[1rem] py-[.3rem] duration-300 ease hover:scale-[.98] hover:opacity-[.7]">
+                    Logout
+                </button>
+            </form>
         </div>
-    </header>
-    <div class="w-full h-[93vh] flex flex-col justify-center items-center gap-[1rem]">
-        <h1 class="text-black font-[700] text-[5rem]">Philippine Precision Care</h1>
-        <h1 class="text-black font-[700] text-[2rem]">Welcome to Dashboard!</h1>
-    </div>
-    <div class="w-[70%] h-full flex justify-end items-center gap-[2rem]">
-        <div class="hr relative">
-            <button class="text-[1rem] font-[400] text-black">
-                Human Resource
-            </button>
-            <div
-                class="hr-content absolute top-[1.5rem] w-[13rem] left-[-2rem] hidden bg-[#a30000] rounded-xl py-[1rem] px-[1rem]">
-                <a href=""
-                    class="text-[.9rem] font-[400] text-white font-[600] duration-300 ease hover:bg-black w-full px-[.5rem] py-[.5rem] rounded-xl text-center">
-                    View Employees
-                </a>
-                <a href=""
-                    class="text-[.9rem] font-[400] text-white font-[600] duration-300 ease hover:bg-black w-full px-[.5rem] py-[.5rem] rounded-xl text-center">
-                    Manage Employees
-                </a>
-            </div>
-        </div>
-
-        <div class="hr relative">
-            <button class="text-[1rem] font-[400] text-black">
-                Finance
-            </button>
-            <div
-                class="hr-content absolute top-[1.5rem] w-[13rem] left-[-4rem] hidden bg-[#a30000] rounded-xl py-[1rem] px-[1rem]">
-                <a href=""
-                    class="text-[.9rem] font-[400] text-white font-[600] duration-300 ease hover:bg-black w-full px-[.5rem] py-[.5rem] rounded-xl text-center">
-                    Manage Finance
-                </a>
-            </div>
-        </div>
-
-        <div class="hr relative">
-            <button class="text-[1rem] font-[400] text-black">
-                Appointment
-            </button>
-            <div
-                class="hr-content absolute top-[1.5rem] w-[13rem] left-[-3rem] hidden bg-[#a30000] rounded-xl py-[1rem] px-[1rem]">
-                <a href=""
-                    class="text-[.9rem] font-[400] text-white font-[600] duration-300 ease hover:bg-black w-full px-[.5rem] py-[.5rem] rounded-xl text-center">
-                    Manage Appointments
-                </a>
-            </div>
-        </div>
-
-        <button
-            class="text-[1rem] text-white font-[400] text-black bg-[#a30000] px-[1rem] py-[.3rem] duration-300 ease hover:scale-[.98] hover:opacity-[.7]">
-            <a href="../../backend/logout.php" style="text-decoration: none; color: white;">Logout</a>
-        </button>
-
-
-    </div>
     </header>
     <div class="w-full h-[93vh] flex flex-col justify-center items-center gap-[1rem]">
         <h1 class="text-black font-[700] text-[5rem]">Philippine Precision Care</h1>
