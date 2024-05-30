274dffe678b6e260f7a5ac3dd48f0aff313ee008
Mel <melbraeisantiago@gmail.com>
2024-05-30 11:36:20 +0800

    Pinantay lang image

--- frontend/pages/TeamAndCulture.php
+++ frontend/pages/TeamAndCulture.php
@@ -21,71 +21,84 @@
             <img src='../assets/logo-no-background.png' alt="Logo" class="object-contain w-full h-full">
         </div>
         <div class="navs w-[70%] h-full flex justify-end items-center gap-[.5rem]">
-            <a href="./index.php" class="text-[1rem] font-[400] text-black px-[1.5rem] py-[.4rem] rounded-[1rem] duration-300 ease hover:bg-[#111] hover:bg-opacity-[.6] hover:text-white">
+            <a href="./index.php"
+                class="text-[1rem] font-[400] text-black px-[1.5rem] py-[.4rem] rounded-[1rem] duration-300 ease hover:bg-[#111] hover:bg-opacity-[.6] hover:text-white">
                 Home
             </a>
-            <a href="./Services.php" class="text-[1rem] font-[400] text-black px-[1.5rem] py-[.4rem] rounded-[1rem] duration-300 ease hover:bg-[#111] hover:bg-opacity-[.6] hover:text-white">
+            <a href="./Services.php"
+                class="text-[1rem] font-[400] text-black px-[1.5rem] py-[.4rem] rounded-[1rem] duration-300 ease hover:bg-[#111] hover:bg-opacity-[.6] hover:text-white">
                 Services
             </a>
-            <a href="./TeamAndCulture.php" class="active text-[1rem] font-[400] text-black px-[1.5rem] py-[.4rem] rounded-[1rem] duration-300 ease hover:bg-[#111] hover:bg-opacity-[.6] hover:text-white">
+            <a href="./TeamAndCulture.php"
+                class="active text-[1rem] font-[400] text-black px-[1.5rem] py-[.4rem] rounded-[1rem] duration-300 ease hover:bg-[#111] hover:bg-opacity-[.6] hover:text-white">
                 Team & Culture
             </a>
-            <a href="./Appointment.php" class="text-[1rem] font-[400] text-black px-[1.5rem] py-[.4rem] rounded-[1rem] duration-300 ease hover:bg-[#111] hover:bg-opacity-[.6] hover:text-white">
+            <a href="./Appointment.php"
+                class="text-[1rem] font-[400] text-black px-[1.5rem] py-[.4rem] rounded-[1rem] duration-300 ease hover:bg-[#111] hover:bg-opacity-[.6] hover:text-white">
                 Appointment
             </a>
-            <a href="./ContactUs.php" class="text-[1rem] text-white font-[400] text-black bg-[#a30000] px-[1rem] py-[.3rem] duration-300 ease hover:scale-[.98] hover:opacity-[.7]">
+            <a href="./ContactUs.php"
+                class="text-[1rem] text-white font-[400] text-black bg-[#a30000] px-[1rem] py-[.3rem] duration-300 ease hover:scale-[.98] hover:opacity-[.7]">
                 Contact Us
             </a>
         </div>
     </header>
-    <div class="w-full py-[5rem] bg-[#ffffff] px-[2rem] sm:px-[5rem] md:px-[10rem] lg:px-[20rem] flex flex-col items-center gap-[3rem]">
+    <div
+        class="w-full py-[5rem] bg-[#ffffff] px-[2rem] sm:px-[5rem] md:px-[10rem] lg:px-[20rem] flex flex-col items-center gap-[3rem]">
         <h1 class="text-[#c10000] font-bold text-[2rem] sm:text-[3rem] md:text-[4rem]">Our Team & Culture</h1>
         <div class="w-full flex flex-col items-start gap-[2rem]">
             <div class="w-full flex justify-evenly items-center gap-[2rem]">
                 <div class="w-[24rem] h-[27rem] flex flex-col justify-center items-center gap-[1rem]">
-                    <div class="overflow-hidden w-full h-full rounded-[50%] flex justify-center items-start">
+                    <div class="overflow-hidden w-full h-[20rem] rounded-[50%] flex justify-center items-start">
                         <img src="../assets/Marcasd.png" alt="Marc" class="object-cover w-full h-full">
                     </div>
-                    <h1 class="text-[1.5rem] font-[600]">Dr. Marc Edison</h1>
+                    <h1 class="text-[1.5rem] font-[600]">Dr. Marc Edison Suarez</h1>
                 </div>
-                <div class="w-[24rem] h-[27rem]  flex flex-col justify-center items-center gap-[1rem]">
-                    <div class="overflow-hidden w-full h-full rounded-[50%] flex justify-center items-start">
+                <div class="w-[24rem] h-[27rem] flex flex-col justify-center items-center gap-[1rem]">
+                    <div class="overflow-hidden w-full h-[20rem] rounded-[50%] flex justify-center items-start">
                         <img src="../assets/Tonasd.png" alt="Marc" class="object-cover w-full h-full">
                     </div>
-                    <h1 class="text-[1.5rem] font-[600]">Dr. Ton Yacapin</h1>
+                    <h1 class="text-[1.5rem] font-[600]">Dr. Hamelton Yacapin</h1>
                 </div>
-                <div class="w-[24rem] h-[27rem]  flex flex-col justify-center items-center gap-[1rem]">
-                    <div class="overflow-hidden w-full h-full rounded-[50%] flex justify-center items-start">
+                <div class="w-[24rem] h-[27rem] flex flex-col justify-center items-center gap-[1rem]">
+                    <div class="overflow-hidden w-full h-[20rem] rounded-[50%] flex justify-center items-start">
                         <img src="../assets/Cams.png" alt="Marc" class="object-cover w-full h-full">
                     </div>
                     <h1 class="text-[1.5rem] font-[600]">R.N. Camille Naces</h1>
                 </div>
                 <div class="w-[24rem] h-[27rem] flex flex-col justify-center items-center gap-[1rem]">
-                    <div class="overflow-hidden w-full h-full rounded-[50%] flex justify-center items-start">
+                    <div class="overflow-hidden w-full h-[20rem] rounded-[50%] flex justify-center items-start">
                         <img src="../assets/Melbb.png" alt="Marc" class="object-cover w-full h-full">
                     </div>
-                    <h1 class="text-[1.5rem] font-[600]">Mr. Melbraei Santiago</h1>
+                    <h1 class="text-[1.5rem] font-[600]">Mngr. Melbraei Santiago</h1>
                 </div>
+
 
             </div>
             <h2 class="text-[1.5rem] sm:text-[2rem] md:text-[2.5rem] text-black font-bold mt-[4rem]">Meet Our Team</h2>
             <p class="text-[1rem] sm:text-[1.2rem] md:text-[1.5rem] text-justify">
-                Our team comprises dedicated professionals committed to providing the best healthcare services. With a blend of experienced specialists and passionate young professionals, we ensure top-notch care for all our patients.
+                Our team comprises dedicated professionals committed to providing the best healthcare services. With a
+                blend of experienced specialists and passionate young professionals, we ensure top-notch care for all
+                our patients.
             </p>
             <ul class="list-disc pl-[1.5rem]">
                 <li class="text-[1rem] sm:text-[1.2rem] md:text-[1.5rem]">Dr. Marc Suarez - Chief Medical Officer</li>
                 <li class="text-[1rem] sm:text-[1.2rem] md:text-[1.5rem]">Dr. Ton Yacapin - Head of Diagnostics</li>
                 <li class="text-[1rem] sm:text-[1.2rem] md:text-[1.5rem]">R.N. Camille Naces - Lead Nurse</li>
-                <li class="text-[1rem] sm:text-[1.2rem] md:text-[1.5rem]">Mr. Melbraei Santiago - Laboratory Manager</li>
+                <li class="text-[1rem] sm:text-[1.2rem] md:text-[1.5rem]">Mr. Melbraei Santiago - Laboratory Manager
+                </li>
             </ul>
             <h2 class="text-[1.5rem] sm:text-[2rem] md:text-[2.5rem] text-black font-bold">Our Culture</h2>
             <p class="text-[1rem] sm:text-[1.2rem] md:text-[1.5rem] text-justify">
-                At Philippine Precision Care, we foster a culture of excellence, empathy, and continuous learning. Our team is dedicated to improving healthcare standards and delivering compassionate care to our patients.
+                At Philippine Precision Care, we foster a culture of excellence, empathy, and continuous learning. Our
+                team is dedicated to improving healthcare standards and delivering compassionate care to our patients.
             </p>
         </div>
     </div>
-    <footer class="w-full h-[30dvh] bg-[#a30000] py-[2rem] px-[1rem] sm:px-[2rem] md-px-[3rem] flex flex-col justify-center items-center gap-[1rem]">
-        <h1 class='font-[500] text-[.5rem] sm:text-[.8rem] md:text-[1rem] text-[#fff] tracking-[.5rem] sm:tracking-[.7rem] md:tracking-[1rem]'>
+    <footer
+        class="w-full h-[30dvh] bg-[#a30000] py-[2rem] px-[1rem] sm:px-[2rem] md-px-[3rem] flex flex-col justify-center items-center gap-[1rem]">
+        <h1
+            class='font-[500] text-[.5rem] sm:text-[.8rem] md:text-[1rem] text-[#fff] tracking-[.5rem] sm:tracking-[.7rem] md:tracking-[1rem]'>
             CONNECT WITH US
         </h1>
         <div class="flex gap-[2rem]">
